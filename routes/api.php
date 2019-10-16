6451c0cec9c38cb2eb2a827ce18842a8376ef26e
lukas <kosha.industry@gmail.com>
2019-10-16 17:37:36 +0600

    delete comments

--- routes/api.php
+++ routes/api.php
@@ -11,10 +11,6 @@
 |
 */
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
 Route::post('login', 'Auth\LoginController')->middleware('guest');
 
 // users
