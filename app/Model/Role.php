3c06f7e89cc30cd7017410e84fbfc785af16fc36
lukas <kosha.industry@gmail.com>
2019-10-16 16:18:50 +0600

    generate phpdoc

--- app/Model/Role.php
+++ app/Model/Role.php
@@ -4,6 +4,18 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Model\Role
+ *
+ * @property int $id
+ * @property string $name
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Role newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Role newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Role query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Role whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Role whereName($value)
+ * @mixin \Eloquent
+ */
 class Role extends Model {
   //
 }
--- app/Model/User.php
+++ app/Model/User.php
@@ -6,6 +6,28 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * App\Model\User
+ *
+ * @property int $id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $email
+ * @property string $password
+ * @property int|null $role_id
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\User whereRoleId($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable {
   use Notifiable;
 
