acb04d5085b52737b968a37a76be966d5aebc48f
lukas <kosha.industry@gmail.com>
2019-10-16 17:28:43 +0600

    update login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller {
   /**
@@ -13,11 +14,32 @@ class LoginController extends Controller {
    * @return \Illuminate\Http\Response
    */
   public function __invoke(Request $request) {
-    //$this->validate($request, [
-    //  'email'    => 'required|string|email',
-    //  'password' => 'required|string'
-    //]);
+    $data = $request->json()->all();
+    Validator::make($data, [
+      'email'    => 'required|string|email',
+      'password' => 'required|string'
+    ])->validate();
 
-    dd('valid1');
+    $credentials = request(['email', 'password']);
+
+    if (!$token = auth()->attempt($credentials)) {
+      return response()->json([
+        'error' => 'Invalid email/password supplied'
+      ], 400);
+    }
+
+    return $this->respondWithToken($token);
+  }
+
+  private function respondWithToken($token) {
+    $ttl = auth()->factory()->getTTL(); // in minutes
+    $expires = now()->addMinutes($ttl);
+
+    return response()->json(['token' => $token])->withHeaders([
+      // max requests per hour
+      'X-Rate-Limit'    => config('api.rate_limit'),
+      // date format: https://swagger.io/docs/specification/data-models/data-types/
+      'X-Expires-After' => $expires->format(DATE_RFC3339),
+    ]);
   }
 }
