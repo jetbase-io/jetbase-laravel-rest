8ff509fe60da04d9ab7b484aa52d193dd3c2bfa0
lukas <kosha.industry@gmail.com>
2019-10-16 20:15:44 +0600

    add 'admin' state to Users factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,8 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
+use App\Model\User;
+use App\Model\Role;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -25,3 +26,7 @@ $factory->define(User::class, function (Faker $faker) {
     'password'   => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
   ];
 });
+
+$factory->state(User::class, 'admin', [
+  'role_id' => Role::admin()->id,
+]);
