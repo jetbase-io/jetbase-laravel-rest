154792b9c89274242f0573a03759eac8b3962439
lukas <kosha.industry@gmail.com>
2019-10-16 18:53:31 +0600

    allow user self change password

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -67,6 +67,10 @@ class UserPolicy {
     return $user->id === $model->id;
   }
 
+  public function changePassword(User $user, User $model) {
+    return $user->id === $model->id;
+  }
+
   /**
    * Determine whether the user can restore the model.
    *
