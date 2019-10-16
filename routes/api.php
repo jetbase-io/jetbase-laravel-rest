9de5ebc2bd15b5a7c42aaec508525c767b8d373a
lukas <kosha.industry@gmail.com>
2019-10-16 18:12:16 +0600

    add slash prefix

--- routes/api.php
+++ routes/api.php
@@ -12,10 +12,10 @@
 */
 
 // auth
-Route::post('login', 'Auth\LoginController')->middleware('guest');
-Route::delete('logout', 'Auth\LogoutController')->middleware('auth');
+Route::post('/login', 'Auth\LoginController')->middleware('guest');
+Route::delete('/logout', 'Auth\LogoutController')->middleware('auth');
 
 // users
-Route::post('users', 'UsersController@create')->middleware('auth');
-Route::get('users', 'UsersController@search')->middleware('auth');
-Route::get('users/current', 'UsersController@current')->middleware('auth');
+Route::post('/users', 'UsersController@create')->middleware('auth');
+Route::get('/users', 'UsersController@search')->middleware('auth');
+Route::get('/users/current', 'UsersController@current')->middleware('auth');
