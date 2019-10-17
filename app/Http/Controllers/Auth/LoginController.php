fb15cd4fadb998b5cdc7a77f138a609218b8e145
lukas <kosha.industry@gmail.com>
2019-10-17 17:12:57 +0600

    update indentation

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,40 +6,43 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-class LoginController extends Controller {
-  /**
-   * Handle the incoming request.
-   *
-   * @param \Illuminate\Http\Request $request
-   * @return \Illuminate\Http\Response
-   */
-  public function __invoke(Request $request) {
-    $data = $request->json()->all();
-    Validator::make($data, [
-      'email'    => 'required|string|email',
-      'password' => 'required|string'
-    ])->validate();
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $data = $request->json()->all();
+        Validator::make($data, [
+            'email'    => 'required|string|email',
+            'password' => 'required|string'
+        ])->validate();
 
-    $credentials = request(['email', 'password']);
+        $credentials = request(['email', 'password']);
 
-    if (!$token = auth()->attempt($credentials)) {
-      return response()->json([
-        'error' => 'Invalid email/password supplied'
-      ], 400);
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json([
+                'error' => 'Invalid email/password supplied'
+            ], 400);
+        }
+
+        return $this->respondWithToken($token);
     }
 
-    return $this->respondWithToken($token);
-  }
+    private function respondWithToken($token)
+    {
+        $ttl = auth()->factory()->getTTL(); // in minutes
+        $expires = now()->addMinutes($ttl);
 
-  private function respondWithToken($token) {
-    $ttl = auth()->factory()->getTTL(); // in minutes
-    $expires = now()->addMinutes($ttl);
-
-    return response()->json(['token' => $token])->withHeaders([
-      // max requests per hour
-      'X-Rate-Limit'    => config('api.rate_limit'),
-      // date format: https://swagger.io/docs/specification/data-models/data-types/
-      'X-Expires-After' => $expires->format(DATE_RFC3339),
-    ]);
-  }
+        return response()->json(['token' => $token])->withHeaders([
+            // max requests per hour
+            'X-Rate-Limit'    => config('api.rate_limit'),
+            // date format: https://swagger.io/docs/specification/data-models/data-types/
+            'X-Expires-After' => $expires->format(DATE_RFC3339),
+        ]);
+    }
 }
