3fb00ef8d885fe692131f3395b55feec8ea2b580
lukas <kosha.industry@gmail.com>
2019-10-18 15:05:08 +0600

    coding standard

--- tests/Feature/Api/GetUserByIdTest.php
+++ tests/Feature/Api/GetUserByIdTest.php
@@ -6,66 +6,71 @@ use App\Model\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 
-class GetUserByIdTest extends ApiTestCase {
+class GetUserByIdTest extends ApiTestCase
+{
 
-  use RefreshDatabase;
+    use RefreshDatabase;
 
-  public function testAsGuest() {
-    $response = $this->json('GET', '/users/1');
-    $response->assertStatus(401); // not authorized
-  }
+    public function testAsGuest()
+    {
+        $response = $this->json('GET', '/users/1');
+        $response->assertStatus(401); // not authorized
+    }
 
-  public function testNotFound() {
-    $user = factory(User::class)->create();
+    public function testNotFound()
+    {
+        $user = factory(User::class)->create();
 
-    // perform login
-    $token = $this->login($user->email);
+        // perform login
+        $token = $this->login($user->email);
 
-    // try get user by not existing id
-    $id = $user->id + 1;
-    $response = $this->json('GET', "/users/$id", [], [
-      'Authorization' => 'Bearer ' . $token
-    ]);
+        // try get user by not existing id
+        $id = $user->id + 1;
+        $response = $this->json('GET', "/users/$id", [], [
+            'Authorization' => 'Bearer ' . $token
+        ]);
 
-    $response->assertStatus(404);
-  }
+        $response->assertStatus(404);
+    }
 
-  public function testSuccess() {
-    $user = factory(User::class)->create();
+    public function testSuccess()
+    {
+        $user = factory(User::class)->create();
 
-    // perform login
-    $token = $this->login($user->email);
+        // perform login
+        $token = $this->login($user->email);
 
-    // try get user by existing id
-    $response = $this->json('GET', "/users/$user->id", [], [
-      'Authorization' => 'Bearer ' . $token
-    ]);
+        // try get user by existing id
+        $response = $this->json('GET', "/users/$user->id", [], [
+            'Authorization' => 'Bearer ' . $token
+        ]);
 
-    $response->assertStatus(200);
-    $responseUser = $response->json();
+        $response->assertStatus(200);
+        $responseUser = $response->json();
 
-    $this->assertIsArray($responseUser);
-    $this->assertArrayHasKey('id', $responseUser);
-    $this->assertEquals($user->id, $responseUser['id']);
-  }
+        $this->assertIsArray($responseUser);
+        $this->assertArrayHasKey('id', $responseUser);
+        $this->assertEquals($user->id, $responseUser['id']);
+    }
 
-  public function testGetNotMe() {
-    $user1 = factory(User::class)->create();
-    $user2 = factory(User::class)->create();
+    public function testGetNotMe()
+    {
+        $user1 = factory(User::class)->create();
+        $user2 = factory(User::class)->create();
 
-    // perform login by user1
-    $token = $this->login($user1->email);
+        // perform login by user1
+        $token = $this->login($user1->email);
 
-    // try get user2
-    $response = $this->json('GET', "/users/$user2->id", [], [
-      'Authorization' => 'Bearer ' . $token
-    ]);
+        // try get user2
+        $response = $this->json('GET', "/users/$user2->id", [], [
+            'Authorization' => 'Bearer ' . $token
+        ]);
 
-    $response->assertStatus(200);
-    $responseUser = $response->json();
+        $response->assertStatus(200);
+        $responseUser = $response->json();
 
-    $this->assertIsArray($responseUser);
-    $this->assertArrayHasKey('id', $responseUser);
-    $this->assertEquals($user2->id, $responseUser['id']);
-  }
+        $this->assertIsArray($responseUser);
+        $this->assertArrayHasKey('id', $responseUser);
+        $this->assertEquals($user2->id, $responseUser['id']);
+    }
 }
