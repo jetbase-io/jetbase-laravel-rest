b388da43a5bfd2998dfd37321463c7f330e8050f
lukas <kosha.industry@gmail.com>
2019-10-18 15:19:19 +0600

    coding standard

--- app/Http/Requests/BaseRequest.php
+++ app/Http/Requests/BaseRequest.php
@@ -4,13 +4,16 @@ namespace App\Http\Requests;
 
 use Illuminate\Http\Request;
 
-class BaseRequest extends Request {
+class BaseRequest extends Request
+{
 
-  public function expectsJson() {
-    return true;
-  }
+    public function expectsJson()
+    {
+        return true;
+    }
 
-  public function wantsJson() {
-    return true;
-  }
+    public function wantsJson()
+    {
+        return true;
+    }
 }
