2d9f5cc4a4120462548850b850b7f9a63680b39d
lukas <kosha.industry@gmail.com>
2019-10-18 15:18:16 +0600

    coding standard

--- app/helpers.php
+++ app/helpers.php
@@ -1,9 +1,10 @@
 <?php
 
-function validRFC3339Date($date) {
-  if (DateTime::createFromFormat(DateTime::RFC3339, $date) === false) {
-    return false;
-  } else {
-    return true;
-  }
+function validRFC3339Date($date)
+{
+    if (DateTime::createFromFormat(DateTime::RFC3339, $date) === false) {
+        return false;
+    } else {
+        return true;
+    }
 }
