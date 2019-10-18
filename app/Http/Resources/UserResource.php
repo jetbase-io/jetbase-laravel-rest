404d3c8947865396b298ee27a0264954216bd636
lukas <kosha.industry@gmail.com>
2019-10-18 15:19:00 +0600

    coding standard

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -4,21 +4,24 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserResource extends JsonResource {
-  /**
-   * Transform the resource into an array.
-   *
-   * @param \Illuminate\Http\Request $request
-   * @return array
-   */
-  public function toArray($request) {
-    return [
-      'id'         => $this->id,
-      'first_name' => $this->first_name,
-      'last_name'  => $this->last_name,
-      'email'      => $this->email,
-      'password'   => "<HASHED>",
-      'role_id'    => (int) $this->role_id,
-    ];
-  }
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'         => $this->id,
+            'first_name' => $this->first_name,
+            'last_name'  => $this->last_name,
+            'email'      => $this->email,
+            'password'   => "<HASHED>",
+            'role_id'    => (int) $this->role_id,
+        ];
+    }
 }
