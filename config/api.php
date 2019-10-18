5b59d30c5ae6b2cb427ccf8d836a0971f7b8ced3
lukas <kosha.industry@gmail.com>
2019-10-18 15:38:39 +0600

    coding standard

--- config/api.php
+++ config/api.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-  'version'         => 1,
-  'endpoint_prefix' => '/api/v1',
-  'rate_limit'      => 3600 // requests limit per hour (max 1 req / 1 second)
+    'version'         => 1,
+    'endpoint_prefix' => '/api/v1',
+    'rate_limit'      => 3600 // requests limit per hour (max 1 req / 1 second)
 ];
